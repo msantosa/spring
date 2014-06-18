5382df0c3758f2be0e9b263853dbc9e7d1af241e
msantosa <millan.santos.arias@everis.com>
2014-06-18 18:30:23 +0200

    Pruebas

---
+++ spring-jobeet/src/main/java/org/jobeet/controller/indexController.java
@@ -0,0 +1,32 @@
+package org.jobeet.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class indexController {
+	
+	private static final Logger logger = Logger.getLogger(indexController.class);
+	
+	@RequestMapping(value="/")
+	public String index(){
+		if(logger.isDebugEnabled()){
+			logger.debug("Hemos entrado en index");
+		}
+		
+		return "index"; 
+	}
+	
+	@RequestMapping(value="/hello", method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+		if(logger.isDebugEnabled()){
+			logger.debug("Hemos entrado en hello");
+		}
+		model.addAttribute("message", "Hello Spring MVC Framework!");
+		return "hello";
+	}
+
+}
---
+++ spring-jobeet/src/main/java/org/jobeet/model/Afiliado.java
@@ -0,0 +1,71 @@
+package org.jobeet.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Afiliado")
+public class Afiliado {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="url", length=255, nullable=false)
+	String url;
+	
+	@Column(name="email" , length=255 , nullable=false, unique=true)
+	String email;
+	
+	@Column(name="token" , length=255, nullable=false)
+	String token;
+	
+	@Column(name="is_active", nullable=false)
+	boolean is_active;
+	
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public boolean isIs_active() {
+		return is_active;
+	}
+
+	public void setIs_active(boolean is_active) {
+		this.is_active = is_active;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
