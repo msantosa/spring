ceb2aeaf7e12afb62b0b54e08bdbb05d6707811f
msantosa <millan.santos.arias@everis.com>
2014-07-17 09:45:35 +0200

    DAO

--- spring-jobeet/src/main/java/org/jobeet/dao/impl/CategoryDaoImpl.java
+++ spring-jobeet/src/main/java/org/jobeet/dao/impl/CategoryDaoImpl.java
@@ -2,12 +2,14 @@ package org.jobeet.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
 import org.jobeet.dao.ICategoryDao;
 import org.jobeet.model.JobeetCategory;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 public class CategoryDaoImpl extends HibernateDaoSupport implements ICategoryDao{
 	
+	private SessionFactory sessionFactory;
 	
 	public void addCategory(JobeetCategory categoria){
 		
